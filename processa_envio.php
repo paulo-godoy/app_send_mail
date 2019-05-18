e873db0967ae31ac72c755a443988284930d7155
paulo-godoy <pauloricardo.godoy@yahoo.com.br>
2019-05-17 23:13:48 -0300

    finalizando back-end

--- processa_envio.php
+++ processa_envio.php
@@ -7,78 +7,132 @@
     require "./biblioteca/PHPMailer/SMTP.php";
 
     use PHPMailer\PHPMailer\PHPMailer;
-    use PHPMailer\PHPMailer\Exception;
-    //print_r($_POST);
+	use PHPMailer\PHPMailer\Exception;
 
-    class Mensagem {
-        private $para = null;
-        private $assunto = null;
-        private $mensagem = null;
-        
-        public function __get($atributo) {
-            return $this->$atributo;
-        }
+	//print_r($_POST);
 
-        public function __set($atributo, $valor) {
-            $this->atributo = $valor;
-        }
+	class Mensagem {
+		private $para = null;
+		private $assunto = null;
+		private $mensagem = null;
+		public $status = array( 'codigo_status' => null, 'descricao_status' => '');
 
-        public function mensagemValida(){
+		public function __get($atributo) {
+			return $this->$atributo;
+		}
 
-            if(empty($this->para) || empty($this->assunto) || empty($this->mensagem)) {
-                return false;
-            }
+		public function __set($atributo, $valor) {
+			$this->$atributo = $valor;
+		}
 
-            return true;
-        }
-    }
+		public function mensagemValida() {
+			if(empty($this->para) || empty($this->assunto) || empty($this->mensagem)) {
+				return false;
+			}
 
-    $mensagem = new Mensagem();
-    $mensagem->__set('para', $_POST['para']);
-    $mensagem->__set('assunto', $_POST['assunto']);
-    $mensagem->__set('mensagem', $_POST['mensagem']);
+			return true;
+		}
+	}
 
-    // print_r($mensagem);
-    
-    if(!$mensagem->mensagemValida()) {
-        echo 'Mensagem inválida!';
-        die();
-    }
+	$mensagem = new Mensagem();
 
-    $mail = new PHPMailer(true);
-    try {
-        //Server settings
-        $mail->SMTPDebug = 2;                                 // Enable verbose debug output
-        $mail->isSMTP();                                      // Set mailer to use SMTP
-        $mail->Host = 'smtp1.example.com;smtp2.example.com';  // Specify main and backup SMTP servers
-        $mail->SMTPAuth = true;                               // Enable SMTP authentication
-        $mail->Username = 'user@example.com';                 // SMTP username
-        $mail->Password = 'secret';                           // SMTP password
-        $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-        $mail->Port = 587;                                    // TCP port to connect to
+	$mensagem->__set('para', $_POST['para']);
+	$mensagem->__set('assunto', $_POST['assunto']);
+	$mensagem->__set('mensagem', $_POST['mensagem']);
 
-        //Recipients
-        $mail->setFrom('from@example.com', 'Mailer');
-        $mail->addAddress('joe@example.net', 'Joe User');     // Add a recipient
-        $mail->addAddress('ellen@example.com');               // Name is optional
-        $mail->addReplyTo('info@example.com', 'Information');
-        $mail->addCC('cc@example.com');
-        $mail->addBCC('bcc@example.com');
+	//print_r($mensagem);
 
-        //Attachments
-        $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-        $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+	if(!$mensagem->mensagemValida()) {
+		echo 'Mensagem não é válida';
+		header('Location: index.php');
+	}
 
-        //Content
-        $mail->isHTML(true);                                  // Set email format to HTML
-        $mail->Subject = 'Here is the subject';
-        $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-        $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+	$mail = new PHPMailer(true);
+	try {
+	    //Server settings
+	    $mail->SMTPDebug = false;                                 // Enable verbose debug output
+	    $mail->isSMTP();                                      // Set mailer to use SMTP
+	    $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+	    $mail->SMTPAuth = true;                               // Enable SMTP authentication
+	    $mail->Username = 'pauloricardo.paulogodoy@gmail.com';                 // SMTP username
+	    $mail->Password = 'ricardo80';                           // SMTP password
+	    $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+	    $mail->Port = 587;                                    // TCP port to connect to
 
-        $mail->send();
-        echo 'Message has been sent';
+	    //Recipients
+	    $mail->setFrom('pauloricardo.paulogodoy@gmail.com', 'Paulo Godoy Remente');
+	    $mail->addAddress($mensagem->__get('para'));     // Add a recipient
+	    //$mail->addReplyTo('info@example.com', 'Information');
+	    //$mail->addCC('cc@example.com');
+	    //$mail->addBCC('bcc@example.com');
 
-    } catch (Exception $e) {
-        echo 'Não foi possivel enviar seu e-mail tente mais tarde.';
-        echo 'Mailer Error: ' . $mail->ErrorInfo;
-        }
+	    //Attachments
+	    //$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+	    //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+
+	    //Content
+	    $mail->isHTML(true);                                  // Set email format to HTML
+	    $mail->Subject = $mensagem->__get('assunto');
+	    $mail->Body    = $mensagem->__get('mensagem');
+	    $mail->AltBody = 'É necessário utilizar um client que suporte HTML para ter acesso total ao conteúdo dessa mensagem';
+
+	    $mail->send();
+
+	    $mensagem->status['codigo_status'] = 1;
+	    $mensagem->status['descricao_status'] = 'E-mail enviado com sucesso';
+	
+	} catch (Exception $e) {
+
+		$mensagem->status['codigo_status'] = 2;
+	    $mensagem->status['descricao_status'] = 'Não foi possível enviar este e-mail! Por favor tente novamente mais tarde. Detalhes do erro: ' . $mail->ErrorInfo;
+
+	    //alguma lógica que armazene o erro para posterior análise por parte do programador
+	}
+?>
+
+<html>
+	<head>
+		<meta charset="utf-8" />
+    	<title>App Mail Send</title>
+
+    	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	</head>
+
+	<body>
+
+		<div class="container">
+			<div class="py-3 text-center">
+				<img class="d-block mx-auto mb-2" src="imagens/logo.png" alt="" width="72" height="72">
+				<h2>Send Mail</h2>
+				<p class="lead">Seu app de envio de e-mails particular!</p>
+			</div>
+
+			<div class="row">
+				<div class="col-md-12">
+
+					<? if($mensagem->status['codigo_status'] == 1) { ?>
+
+						<div class="container">
+							<h1 class="display-4 text-success">Sucesso</h1>
+							<p><?= $mensagem->status['descricao_status'] ?></p>
+							<a href="index.php" class="btn btn-success btn-lg mt-5 text-white">Voltar</a>
+						</div>
+
+					<? } ?>
+
+					<? if($mensagem->status['codigo_status'] == 2) { ?>
+
+						<div class="container">
+							<h1 class="display-4 text-danger">Mensagem não enviada!</h1>
+							<p><?= $mensagem->status['descricao_status'] ?></p>
+							<a href="index.php" class="btn btn-success btn-lg mt-5 text-white">Voltar</a>
+						</div>
+
+					<? } ?>
+
+				</div>
+			</div>
+		</div>
+
+	</body>
+</html>
